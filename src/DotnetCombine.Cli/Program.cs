6d3f1122875ba2667a87182091f8281bc7cce21f
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2020-11-17 00:56:41 +0100

    Tweak help text.

--- src/DotnetCombine.Cli/Program.cs
+++ src/DotnetCombine.Cli/Program.cs
@@ -1,6 +1,9 @@
 ﻿using CommandLine;
+using CommandLine.Text;
 using DotnetCombine.Options;
 using DotnetCombine.Services;
+using System;
+using System.Reflection;
 
 namespace DotnetCombine.Cli
 {
@@ -8,10 +11,68 @@ namespace DotnetCombine.Cli
     {
         public static int Main(string[] args)
         {
-            return Parser.Default.ParseArguments<ZipOptions>(args)
+            var parser = new Parser(config => config.HelpWriter = null);
+
+            var result = parser.ParseArguments<CombineOptions, ZipOptions>(args);
+
+            return result
+                .WithNotParsed(_ => DisplayHelp(result))
                 .MapResult(
                   (ZipOptions options) => new Compressor().Run(options),
                   _ => 1);
         }
+
+        private static void DisplayHelp(ParserResult<object> result)
+        {
+            var helpText = HelpText.AutoBuild(result, helpText =>
+            {
+                var assembly = Assembly.GetEntryAssembly();
+                helpText.Heading = $"{assembly?.GetName().Name} {assembly?.GetName().Version}";
+                helpText.Copyright = "Created by Eduardo Cáceres - https://github.com/eduherminio/dotnet-combine";
+                helpText.MaximumDisplayWidth = 120;
+                helpText.AddNewLineBetweenHelpSections = true;
+                helpText.AdditionalNewLineAfterOption = true;
+                helpText.OptionComparison = OrderWithValuesFirst;
+
+                helpText.AddPreOptionsLine(result.TypeInfo.Current == typeof(ZipOptions)
+                    ? "Usage: dotnet-combine zip <INPUT> [options]"
+                    : "Usage: dotnet-combine single-file <INPUT> [options]");
+
+                return HelpText.DefaultParsingErrorsHandler(result, helpText);
+            }, e => e);
+
+            Console.WriteLine(helpText);
+        }
+
+        private static readonly Comparison<ComparableOption> OrderWithValuesFirst = (ComparableOption attr1, ComparableOption attr2) =>
+        {
+            if (attr1.IsOption && attr2.IsOption)
+            {
+                if (attr1.Required && !attr2.Required)
+                {
+                    return -1;
+                }
+                else if (!attr1.Required && attr2.Required)
+                {
+                    return 1;
+                }
+
+                return attr1.Index > attr2.Index
+                    ? 1
+                    : -1;
+            }
+            else if (attr1.IsOption && attr2.IsValue)
+            {
+                return 1;
+            }
+            else if (attr1.IsValue && attr2.IsOption)
+            {
+                return -1;
+            }
+            else
+            {
+                return 1;
+            }
+        };
     }
 }
---
+++ src/DotnetCombine/Options/CombineOptions.cs
@@ -0,0 +1,11 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+
+namespace DotnetCombine.Options
+{
+    [Verb("single-file", isDefault: false, HelpText = "Combines multiple source code files (.cs) into a single one")]
+    public class CombineOptions
+    {
+    }
+}
