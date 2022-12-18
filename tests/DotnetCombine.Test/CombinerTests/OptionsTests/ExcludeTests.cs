38d682989507b33f6c9998d5054b410cb2b5acf0
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2022-12-18 02:32:31 +0100

    Revert extra exclusions: the point is actually to test that they're not needed when a filname is provided

--- tests/DotnetCombine.Test/CombinerTests/OptionsTests/ExcludeTests.cs
+++ tests/DotnetCombine.Test/CombinerTests/OptionsTests/ExcludeTests.cs
@@ -112,7 +112,7 @@ public class ExcludeTests : BaseCombinerTests
 
         var options = new CombineOptions()
         {
-            ExcludedItems = new[] { expectedOutputFileName },
+            ExcludedItems = Array.Empty<string>(),
             Output = expectedOutputFile,
             OverWrite = true,
             Input = inputDir,
@@ -139,7 +139,7 @@ public class ExcludeTests : BaseCombinerTests
         // Act - without having output file deleted
         options = new CombineOptions()
         {
-            ExcludedItems = new[] { expectedOutputFileName },
+            ExcludedItems = Array.Empty<string>(),
             Output = expectedOutputFile,
             OverWrite = true,
             Input = inputDir,
